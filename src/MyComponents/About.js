60bb690378a412597629f24578a92fbeb58ba663
Devdar-hub <79082308+Devdar-hub@users.noreply.github.com>
2021-06-18 17:10:13 +0530

    Create About.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div>
+            This is a about page
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, nisi et minima suscipit, iste ex reiciendis consequatur voluptatum expedita, eos doloremque voluptas dignissimos delectus non voluptates nulla quia eum. Blanditiis?</p>
+        </div>
+    )
+}
