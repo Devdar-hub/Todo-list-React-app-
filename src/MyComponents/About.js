89fd8c2bd5d2437018a6e0bc3a63bdce8a9882db
Darshan Bhatt <79082308+Devdar-hub@users.noreply.github.com>
2021-06-19 14:41:41 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,11 @@ import React from 'react';
 export const About = () => {
     return (
         <div>
-            This is a about page
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, nisi et minima suscipit, iste ex reiciendis consequatur voluptatum expedita, eos doloremque voluptas dignissimos delectus non voluptates nulla quia eum. Blanditiis?</p>
+            This is a about Todo-List
+            <p>ON the HOME page of this site there are two section as follow:-
+                1. Todo Title - In this section you can add the title of your work eg. Visit Market.
+                2. Description - In this section you can add description of todo title eb. Buy Vegetables , fruits , juices etc
+            </p>
         </div>
     )
 }
